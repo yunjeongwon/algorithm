e85ba33a37e57c54ede27a855baf681fc5303d72
윤정원 <yunjwon94@gmail.com>
2023-01-29 13:52:37 +0900

    update 0129 14501

--- 2023/0129/14501.py
+++ 2023/0129/14501.py
@@ -1,3 +1,4 @@
+# bottom up
 import sys
 input = sys.stdin.readline
 
@@ -16,3 +17,25 @@ for i in range(1, n + 1):
             dp[j] = dp[i] + p[i]
 
 print(dp[n + 1])
+
+# top-down
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+t = [0]
+p = [0]
+dp = [0] * (n + 2)
+for _ in range(n):
+    ti, pi = map(int, input().split())
+    t.append(ti)
+    p.append(pi)
+
+for i in range(n, 0, -1):
+    next = i + t[i]
+    if next > n + 1:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], dp[next] + p[i])
+
+print(dp[1])
