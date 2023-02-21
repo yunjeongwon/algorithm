aea1c81da6f3112ac53d027a861dd91af199098b
yunjeongwon <76723987+yunjeongwon@users.noreply.github.com>
2023-02-21 14:50:03 +0900

    [Silver II] Title: 상자넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.py
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -4,10 +4,19 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1] * n
-for i in range(n):
-    for j in range(i):
-        if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp))
+dp = []
+dp.append(arr[0])
+ans = 0
+for i in range(1, n):
+    if dp[-1] >= arr[i]:
+        idx = 0
+        for j in range(len(dp)):
+            if dp[j] >= arr[i]:
+                idx = j
+                break
+        dp[idx] = arr[i]
+    else:
+        dp.append(arr[i])
+    ans = max(ans, len(dp))
+# print(dp)
+print(ans)
